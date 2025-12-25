936b9a1338cea3b4d116d6650d477250149de032
shiraaki888 <shiraaki888@gmail.com>
2025-12-25 19:31:04 +0200

    update1

--- src/Ex2/Index2D.java
+++ src/Ex2/Index2D.java
@@ -1,6 +1,10 @@
 package Ex2;
 
+import java.util.Objects;
+
 public class Index2D implements Pixel2D {
+
+
     private int x;
     private int y;
     private Pixel2D pixel2D;
@@ -23,7 +27,9 @@ public class Index2D implements Pixel2D {
 
     @Override
     public double distance2D(Pixel2D p2) {
-        return Math.sqrt(x*p2.getX() + y*p2.getY());
+        int dx=x-p2.getX();
+        int dy=y-p2.getY();
+        return Math.sqrt(dx*dx + dy*dy);
     }
 
     @Override
@@ -34,9 +40,14 @@ public class Index2D implements Pixel2D {
     }
 
     @Override
-    public boolean equals(Object p) {
-        boolean ans = true;
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Index2D index2D = (Index2D) o;
+        return x == index2D.x && y == index2D.y;
+    }
 
-        return ans;
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
